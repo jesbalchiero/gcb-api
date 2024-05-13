a655ec9024d6fe41e90684f9f746f8b98b639fe1
jesbalchiero <62852809+jesbalchiero@users.noreply.github.com>
2024-05-13 13:57:42 -0300

    chore(pipes): adding pipes and class-validation

--- src/movies/dto/category.dto.ts
+++ src/movies/dto/category.dto.ts
@@ -1,7 +1,17 @@
+import { IsArray, IsOptional, IsString, ValidateNested } from 'class-validator';
 import { MovieDto } from './movie.dto';
+import { Type } from 'class-transformer';
 
 export class CategoryDto {
+  @IsOptional()
   id?: number;
+
+  @IsString()
   name: string;
+
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true }) // Validação dos objetos dentro do array
+  @Type(() => MovieDto)
   movies?: MovieDto[];
 }
--- src/movies/dto/movie.dto.ts
+++ src/movies/dto/movie.dto.ts
@@ -1,16 +1,66 @@
+import {
+  IsString,
+  IsOptional,
+  IsNumber,
+  IsPositive,
+  IsDateString,
+  ArrayMinSize,
+  IsArray,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
 import { CategoryDto } from './category.dto';
 import { ActorDto } from './actor.dto';
 
 export class MovieDto {
+  @IsOptional()
   id?: number;
+
+  @IsString()
   title: string;
+
+  @IsOptional()
+  @IsString()
   overview?: string;
+
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
   rating?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
   duration?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
   releaseYear?: number;
+
+  @IsOptional()
+  @IsString()
   language?: string;
+
+  @IsOptional()
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true }) // Validação dos objetos dentro do array
+  @Type(() => CategoryDto)
   categories?: CategoryDto[];
+
+  @IsOptional()
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true }) // Validação dos objetos dentro do array
+  @Type(() => ActorDto)
   actors?: ActorDto[];
+
+  @IsOptional()
+  @IsDateString()
   createdAt?: Date;
+
+  @IsOptional()
+  @IsDateString()
   updatedAt?: Date;
 }
