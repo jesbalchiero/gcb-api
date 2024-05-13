e96018112f1efe53309141d88eb306ac75fc7a71
jesbalchiero <62852809+jesbalchiero@users.noreply.github.com>
2024-05-13 13:43:47 -0300

    feat(movies):  findAll and findOne

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -8,39 +8,58 @@ export class MoviesService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(movieDto: MovieDto) {
-    // Mapear os dados do CreateMovieDto para o formato esperado pelo Prisma
-    const data = {
-      title: movieDto.title,
-      overview: movieDto.overview,
-      rating: movieDto.rating,
-      duration: movieDto.duration,
-      releaseYear: movieDto.releaseYear,
-      language: movieDto.language,
-      categories: {
-        connect: movieDto.categories.map((category) => ({ id: category.id })),
-      },
-      actors: {
-        connect: movieDto.actors.map((actor) => ({ id: actor.id })),
-      },
-      createdAt: movieDto.createdAt,
-      updatedAt: movieDto.updatedAt,
-    };
-
-    // Criar o filme usando o Prisma
     try {
-      const movie = await this.prisma.movie.create({ data });
-      return movie;
+      const data = {
+        title: movieDto.title,
+        overview: movieDto.overview,
+        rating: movieDto.rating,
+        duration: movieDto.duration,
+        releaseYear: movieDto.releaseYear,
+        language: movieDto.language,
+        categories: {
+          connect: movieDto.categories.map((category) => ({ id: category.id })),
+        },
+        actors: {
+          connect: movieDto.actors.map((actor) => ({ id: actor.id })),
+        },
+        createdAt: movieDto.createdAt,
+        updatedAt: movieDto.updatedAt,
+      };
+
+      const createdMovie = await this.prisma.movie.create({
+        data,
+      });
+
+      return createdMovie;
     } catch (e) {
-      throw new ExceptionsHandler(e);
+      throw new Error(`Error creating the movie: ${e.message}`);
     }
   }
 
-  findAll() {
-    return `This action returns all movies`;
+  async findAll() {
+    try {
+      return await this.prisma.movie.findMany({
+        orderBy: {
+          id: 'desc',
+        },
+      });
+    } catch (e) {
+      throw new Error(`Error finding all movies: ${e.message}`);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} movie`;
+  async findOne(id: number) {
+    try {
+      const user = await this.prisma.movie.findUnique({
+        where: {
+          id: id,
+        },
+      });
+
+      return user;
+    } catch (e) {
+      throw new Error(`Error finding the movie by ID: ${e.message}`);
+    }
   }
 
   async update(id: number, movieDto: MovieDto) {
